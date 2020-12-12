e390ca696bf7d82784726e237ec3db690a3c3688
Mohammed Saber <mohammed_saber9595@hotmail.com>
2020-12-12 23:04:27 +0000

    Initial commit with Login system basics

---
+++ Login - Register.py
@@ -0,0 +1,21 @@
+def login():
+    pass
+def register():
+    print("Register Now!")
+    username = input("Please input a username: ")
+    password = input("Please choose a password: ")
+    email = input("Please input your email: ")
+
+def begin():
+    global selection
+    print("Welcomer to Artiviewer")
+    option = input("Do you have an account or would you like to register?")
+    option = option.lower()
+    if(option!= "login" and option!="register"):
+        begin()
+    if(option == "login"):
+        login()
+    else:
+        register()
+
+begin()
