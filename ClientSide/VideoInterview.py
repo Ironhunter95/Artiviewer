8c3814173dc8f806fd210842a6bd7bc225f4082b
Mohammed Aboukhatwa <ironhunter90@gmail.com>
2021-05-05 16:40:49 +0100

    Moved audio recorder class to it's own script

--- ClientSide/VideoInterview.py
+++ ClientSide/VideoInterview.py
@@ -10,6 +10,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.uic import loadUi
+from AudioRecorder import AudioRecorder
 
 class VideoInterview(QMainWindow):
     def __init__(self):
@@ -150,57 +151,6 @@ class VideoInterview(QMainWindow):
                                                     "                                                font:16px bold;}")
 
 
-class AudioRecorder():
-
-    # Audio class based on pyAudio and Wave
-    def __init__(self):
-
-        self.open = True
-        self.rate = 44100
-        self.frames_per_buffer = 1024
-        self.channels = 2
-        self.format = pyaudio.paInt16
-        self.audio_filename = "temp_audio.wav"
-        self.audio = pyaudio.PyAudio()
-        self.stream = self.audio.open(format=self.format,
-                                      channels=self.channels,
-                                      rate=self.rate,
-                                      input=True,
-                                      frames_per_buffer=self.frames_per_buffer)
-        self.audio_frames = []
-
-    # Audio starts being recorded
-    def record(self):
-
-        self.stream.start_stream()
-        while (self.open == True):
-            data = self.stream.read(self.frames_per_buffer)
-            self.audio_frames.append(data)
-            if self.open == False:
-                break
-
-    # Finishes the audio recording therefore the thread too
-    def stop(self):
-
-        if self.open == True:
-            self.open = False
-            self.stream.stop_stream()
-            self.stream.close()
-            self.audio.terminate()
-
-            waveFile = wave.open(self.audio_filename, 'wb')
-            waveFile.setnchannels(self.channels)
-            waveFile.setsampwidth(self.audio.get_sample_size(self.format))
-            waveFile.setframerate(self.rate)
-            waveFile.writeframes(b''.join(self.audio_frames))
-            waveFile.close()
-
-        pass
-
-    # Launches the audio recording function using a thread
-    def start(self):
-        audio_thread = threading.Thread(target=self.record)
-        audio_thread.start()
 
 
 def start_AudioRecording():
